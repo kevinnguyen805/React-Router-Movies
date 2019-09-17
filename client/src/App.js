d98fb5b4641906f401cfd1812c294794d687c50f
admin <admin@admins-MacBook-Pro.local>
2019-09-17 11:59:23 -0700

    restarting project - step 1 completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
-import { Route } from 'react-router-dom'
+import {Route} from 'react-router-dom'
+
 import SavedList from './Movies/SavedList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
       <div>
-        <Route exact path="/" component={MovieList}/>
-        <Route path="/movie/:id" component={Movie}/>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={Movie}/>
       </div>
     </div>
   );
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -14,10 +14,10 @@ const MovieList = props => {
           console.error('Server Error', error);
         });
     }
-    
+
     getMovies();
   }, []);
-  
+
   return (
     <div className="movie-list">
       {movies.map(movie => (
